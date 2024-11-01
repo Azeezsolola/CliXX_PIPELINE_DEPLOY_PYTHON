701490ea3ed05c1187d31b827a4f3f3ea7c9b75d
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-11-01 09:45:37 -0400

    AWS-54: Destroying clixx deployment-Azeez_Solola

--- destroy_CliXX.py
+++ destroy_CliXX.py
@@ -444,9 +444,9 @@ response = sns13.delete_topic(
 
 time.sleep(60)
 
-'''
 
-'''
+
+
 
 #---------------------------Detaching internet gateway from vpc -------------------------------------
 ssm=boto3.client('ssm',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
@@ -482,13 +482,13 @@ response = igw.delete_internet_gateway(
 
 time.sleep(300)
 
-
+'''
 #-------------------------------Delete SG--------------------------------------------------------------------------
 ssm=boto3.client('ssm',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = ssm.get_parameter(Name='/myapp/securitygroupid1', WithDecryption=True)
 sg1=response['Parameter']['Value']
 print(sg1)
-
+'''
 ec2=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 security_group = ec2.describe_security_groups(GroupIds=[sg1])['SecurityGroups'][0]
 
@@ -509,6 +509,7 @@ if 'IpPermissionsEgress' in security_group:
     print(f"Removed all outbound rules from security group {sg1}.")
 time.sleep(120)
 '''
+
 SG=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = SG.delete_security_group(
     GroupId=sg1,
